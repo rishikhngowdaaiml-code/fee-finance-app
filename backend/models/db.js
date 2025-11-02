c7443dcefd83a6fe3a637fcd3ffb0569f086ce27
RISHIK H N GOWDA <rishikhngowda_aiml@ksit.edu.in>
2025-11-02 20:09:03 +0530

    Clean rebuild: remove Windows-native sqlite3 for Render Linux

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,4 +1,7 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./models/finance.db');
+const Database = require('better-sqlite3');
 
+// Creates (or opens) the database file
+const db = new Database('./models/finance.db', { verbose: console.log });
+
+// Export the db instance for use in other files
 module.exports = db;
